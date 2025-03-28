63a7ffd69f7ed4d24ff80f0fb74d9b85ea8273b1
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-28 15:30:15 +0000

    Started to add user input to conversion but still needs some rework.

--- Chapter 3 - Common Programming Concepts/temperature_converter/src/main.rs
+++ Chapter 3 - Common Programming Concepts/temperature_converter/src/main.rs
@@ -1,16 +1,52 @@
+use std::io;
+
+// TODO: Call function to ask for temps, then use that to call the reresponding converison function and return back to original main loop.
+
 fn main() {
-    let example_farenheit = 64.0;
-    let example_celsius = 32.0;
+    loop {
+        let mut input = String::new();
 
+        println!("Specify F to convert to Farenheit or C to convert to Celsius");
 
-    println!("{} Farenheit is equal to {} Celsius!", example_farenheit, convert_fahrenheit_to_celsius(example_farenheit));
-    println!("{} Celsius is equal to {} Farenheit!", example_celsius, convert_celsius_to_farenheit(example_celsius));
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input!");
+
+        let input: char = input
+        .trim()
+        .to_lowercase()
+        .parse()
+        .expect("Invalid Input!");
+
+        if input == 'f' {
+            println!("You've chosen to convert to Farenheit!");
+        } else if input == 'c' {
+            println!("You've chosen to convert to Celsius!");
+        } else {
+            println!("Invalid character or nothing entered, try again!")
+        }
+    }
+}
+
+fn ask_for_temperature_to_convert(unit: char) -> f32 {
+    let mut input = String::new();
+    println!("What temperature would you like to convert?");
+    io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input!");
+        
+    let input: f32 = input
+    .trim()
+    .parse()
+    .expect("Invalid Input!");
+
+    input
 }
 
 fn convert_fahrenheit_to_celsius(f: f32) -> f32 {
-    (f - 32.0) * (5.0/9.0)
+    (f - 32.0) * (5.0 / 9.0)
 }
 
 fn convert_celsius_to_farenheit(c: f32) -> f32 {
-    (c * (9.0/5.0)) + 32.0
+    (c * (9.0 / 5.0)) + 32.0
 }
