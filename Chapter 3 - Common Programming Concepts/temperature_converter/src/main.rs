84761b2db85a823cc8ecf9e5cfc252a9b2330f08
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-29 10:16:21 +0000

    Commented out the more experimental code, will revist later. For now the task is completed.

--- Chapter 3 - Common Programming Concepts/temperature_converter/src/main.rs
+++ Chapter 3 - Common Programming Concepts/temperature_converter/src/main.rs
@@ -1,46 +1,52 @@
-use std::io;
+// use std::io;
 
-// TODO: Call function to ask for temps, then use that to call the reresponding converison function and return back to original main loop.
+// fn main() {
+//     loop {
+//         let mut input = String::new();
+
+//         println!("Specify F to convert to Farenheit or C to convert to Celsius");
+
+//         io::stdin()
+//             .read_line(&mut input)
+//             .expect("Failed to read input!");
+
+//         let input: char = input
+//         .trim()
+//         .to_lowercase()
+//         .parse()
+//         .expect("Invalid Input!");
+
+//         if input == 'f' {
+//             println!("You've chosen to convert to Farenheit!");
+//         } else if input == 'c' {
+//             println!("You've chosen to convert to Celsius!");
+//         } else {
+//             println!("Invalid character or nothing entered, try again!")
+//         }
+//     }
+// }
+
+// fn ask_for_temperature_to_convert(unit: char) -> f32 {
+//     let mut input = String::new();
+//     println!("What temperature would you like to convert?");
+//     io::stdin()
+//             .read_line(&mut input)
+//             .expect("Failed to read input!");
+        
+//     let input: f32 = input
+//     .trim()
+//     .parse()
+//     .expect("Invalid Input!");
+
+//     input
+// }
 
 fn main() {
-    loop {
-        let mut input = String::new();
+    let test_celsius = 32.0;
+    let test_farenheit = 96.0;
 
-        println!("Specify F to convert to Farenheit or C to convert to Celsius");
-
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read input!");
-
-        let input: char = input
-        .trim()
-        .to_lowercase()
-        .parse()
-        .expect("Invalid Input!");
-
-        if input == 'f' {
-            println!("You've chosen to convert to Farenheit!");
-        } else if input == 'c' {
-            println!("You've chosen to convert to Celsius!");
-        } else {
-            println!("Invalid character or nothing entered, try again!")
-        }
-    }
-}
-
-fn ask_for_temperature_to_convert(unit: char) -> f32 {
-    let mut input = String::new();
-    println!("What temperature would you like to convert?");
-    io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read input!");
-        
-    let input: f32 = input
-    .trim()
-    .parse()
-    .expect("Invalid Input!");
-
-    input
+    println!("{} Celsius is {} Farenheit.", test_celsius, convert_celsius_to_farenheit(test_celsius));
+    println!("{} Farenheit is {} Celsius.", test_farenheit, convert_fahrenheit_to_celsius(test_farenheit));
 }
 
 fn convert_fahrenheit_to_celsius(f: f32) -> f32 {
