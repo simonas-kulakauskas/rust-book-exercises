f8267e7f8435d0c07a6455519ef302425909c485
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-29 10:33:13 +0000

    Added ability for user to choose what value of the fibonacci sequence they want.

--- Chapter 3 - Common Programming Concepts/fibonacci_number/src/main.rs
+++ Chapter 3 - Common Programming Concepts/fibonacci_number/src/main.rs
@@ -1,16 +1,30 @@
+use std::io;
+
 fn main() {
-    let chosen_number = 15; // Nth value of fibonacci sequence, test value 4.
+    let mut chosen_number = String::new();
+
+    println!("Enter in the number of which you want the nth value...");
+
+    io::stdin()
+        .read_line(&mut chosen_number)
+        .expect("Input couldn't be read!");
+
+    let chosen_number: i32 = chosen_number.trim().parse().expect("Wasn't able to convert to integer.");
+
     let mut i = 0;
 
     let mut first_number = 0;
     let mut second_number = 1;
 
+    println!("{first_number}");
+
     while i < (chosen_number - 1) {
         let new_number = first_number + second_number;
         first_number = second_number;
+        println!("{second_number}");
         second_number = new_number;
         i = i + 1;
     }
 
-    println!("{second_number}");
+    println!("\nThe {chosen_number}th value of the Fibonacci sequence is: {second_number}");
 }
