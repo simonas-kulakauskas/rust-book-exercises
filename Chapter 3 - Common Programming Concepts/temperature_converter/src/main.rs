269611ed7de811d9eaa525dbe5f274a3d53beed2
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-29 14:46:28 +0000

    ask_for_unit now loops when an invalid character is entered.

--- Chapter 3 - Common Programming Concepts/temperature_converter/src/main.rs
+++ Chapter 3 - Common Programming Concepts/temperature_converter/src/main.rs
@@ -5,19 +5,6 @@ fn main() {
         let choose_unit: char = ask_for_unit();
         let choose_temp: f32 = ask_for_temperature(&choose_unit);
 
-        // if choose_unit == 'f' {
-        //     println!(
-        //         "\n{:.1} degrees Celsius is equal to {:.1} Farenheit!\n",
-        //         choose_temp,
-        //         convert_celsius_to_farenheit(choose_temp)
-        //     );
-        // } else if choose_unit == 'c' {
-        //     println!(
-        //         "\n{:.1} degrees Farenheit is equal to {:.1} Celsius!\n",
-        //         choose_temp,
-        //         convert_fahrenheit_to_celsius(choose_temp)
-        //     );
-        // }
         match choose_unit {
             'f' => {
                 println!(
@@ -34,25 +21,34 @@ fn main() {
                 );
             }
             _ => {
-                println!("Value not valid");
+                println!("Value not valid!");
             }
         }
     }
 }
 
 fn ask_for_unit() -> char {
-    let mut choose_unit = String::new();
+    loop {
+        let mut choose_unit = String::new();
+        println!("What unit would you like to convert to? ('f' for Farenheit, 'c' for Celsius");
+        io::stdin()
+            .read_line(&mut choose_unit)
+            .expect("Unexpected unit input!");
 
-    println!("What unit would you like to convert to? ('f' for Farenheit, 'c' for Celsius");
-    io::stdin()
-        .read_line(&mut choose_unit)
-        .expect("Unexpected unit input!");
-
-    choose_unit
-        .trim()
-        .to_lowercase()
-        .parse()
-        .expect("Unable to convert to 'char' type.")
+        let choose_unit: char = choose_unit
+            .trim()
+            .to_lowercase()
+            .parse()
+            .expect("Unable to convert to 'char' type.");
+            
+        if choose_unit == 'f' {
+            return 'f';
+        } else if choose_unit == 'c' {
+            return 'c';
+        } else {
+            println!("Invalid unit, try again...");
+        };
+    };
 }
 
 fn ask_for_temperature(unit: &char) -> f32 {
