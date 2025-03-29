b93359e3e81d9543d4a4c8350288d7bf34437d79
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-29 10:27:10 +0000

    Finished the fibonacci sequence, now going to allow user to enter in a value.

--- Chapter 3 - Common Programming Concepts/fibonacci_number/src/main.rs
+++ Chapter 3 - Common Programming Concepts/fibonacci_number/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let chosen_number = 15; // Nth value of fibonacci sequence, test value 4.
+    let mut i = 0;
+
+    let mut first_number = 0;
+    let mut second_number = 1;
+
+    while i < (chosen_number - 1) {
+        let new_number = first_number + second_number;
+        first_number = second_number;
+        second_number = new_number;
+        i = i + 1;
+    }
+
+    println!("{second_number}");
 }
