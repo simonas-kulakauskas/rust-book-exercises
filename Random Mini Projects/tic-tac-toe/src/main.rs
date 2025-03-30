343a3b441b4c12dc8ab3b0df3234d2e772ef8980
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-30 21:55:01 +0100

    Started a tic-tac-toe project in rust command line.

---
+++ Random Mini Projects/tic-tac-toe/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let row1: [char;3] = [' ', ' ', ' '];
+    let row2: [char;3] = [' ', ' ', ' '];
+    let row3: [char;3] = [' ', ' ', ' '];
+    
+    
+}
+
+fn check_if_cell_empty(cell: &char) -> bool {
+    if cell == &' ' {
+        return true;
+    } else {
+        return false;
+    }
+}
