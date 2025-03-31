8333630f66c3f7c4191a06368b1ebe965f364b32
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-31 14:28:12 +0100

    Added debugging functionality so i can pass through the 'rect1' Rectangle struct and see its values through println! using the `:?` specifier.

--- Chapter 5 - Using structs to structure related data/structs_example_exercise/src/main.rs
+++ Chapter 5 - Using structs to structure related data/structs_example_exercise/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -9,10 +10,7 @@ fn main() {
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+    println!("rect 1 is {:?}", rect1)
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
