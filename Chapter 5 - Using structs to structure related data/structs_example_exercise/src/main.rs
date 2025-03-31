496fa6b3fa0ff494966146a0c3022b3ce43e8737
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-31 14:45:10 +0100

    Added my own method to check if a given rectangle is a square. Also applied cargo formatting.

--- Chapter 5 - Using structs to structure related data/structs_example_exercise/src/main.rs
+++ Chapter 5 - Using structs to structure related data/structs_example_exercise/src/main.rs
@@ -14,6 +14,9 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+    fn is_square(&self) -> bool {
+        self.width == self.height
+    }
     fn square(size: u32) -> Self {
         Self {
             width: size,
@@ -45,25 +48,17 @@ fn main() {
         rect1.area()
     );
 
-    if rect1.width() { // parentheses tell Rust we mean the method 'width', no parentheses means the field 'width'.
-        println!(
-            "The rectangle has a nonzero width of {}.",
-            rect1.width
-        )
+    if rect1.width() {
+        // parentheses tell Rust we mean the method 'width', no parentheses means the field 'width'.
+        println!("The rectangle has a nonzero width of {}.", rect1.width)
     };
 
-    println!(
-        "Can 'rect1' hold 'rect2'? : {}",
-        rect1.can_hold(&rect2)
-    );
-    
-    println!(
-        "Can 'rect1' hold 'rect3'? : {}",
-        rect1.can_hold(&rect3)
-    );
+    println!("Can 'rect1' hold 'rect2'? : {}", rect1.can_hold(&rect2));
 
-    println!(
-        "rect4 is a square: {:?}",
-        rect4
-    );
+    println!("Can 'rect1' hold 'rect3'? : {}", rect1.can_hold(&rect3));
+
+    println!("rect4 is a square: {:?}", rect4);
+
+    println!("Is rect1 a square?: {}", rect1.is_square());
+    println!("Is rect4 a square?: {}", rect4.is_square());
 }
