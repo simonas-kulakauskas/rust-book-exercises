034390844048fae4ca687d9717ebe2f1021cb344
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-31 14:40:18 +0100

    Added another method which takes two arguments, one being itself and the 2nd being another Rectangle type. If the width and height of the self is larger than the other, it'll return true, otherwise false...

--- Chapter 5 - Using structs to structure related data/structs_example_exercise/src/main.rs
+++ Chapter 5 - Using structs to structure related data/structs_example_exercise/src/main.rs
@@ -11,12 +11,25 @@ impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0
     }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
     };
 
     println!(
@@ -30,4 +43,14 @@ fn main() {
             rect1.width
         )
     };
+
+    println!(
+        "Can 'rect1' hold 'rect2'? : {}",
+        rect1.can_hold(&rect2)
+    );
+    
+    println!(
+        "Can 'rect1' hold 'rect3'? : {}",
+        rect1.can_hold(&rect3)
+    );
 }
