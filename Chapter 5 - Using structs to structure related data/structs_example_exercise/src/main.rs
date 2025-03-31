c8bca04249b05251f07cebce3b1b1edb4d8e881a
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-31 14:30:26 +0100

    Used the 'dbg' function rather than the 'println' to output it to the 'stderr' error console stream rather than the 'stdout' standard output stream.

--- Chapter 5 - Using structs to structure related data/structs_example_exercise/src/main.rs
+++ Chapter 5 - Using structs to structure related data/structs_example_exercise/src/main.rs
@@ -5,12 +5,13 @@ struct Rectangle {
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
 
-    println!("rect 1 is {:?}", rect1)
+    dbg!(&rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
