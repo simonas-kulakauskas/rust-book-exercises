7e8bf142ad8cd2f2f2f81864ebb474d526e3633d
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-04-03 12:52:08 +0100

    Wrote up the enum-example

--- Chapter 6 - Enums and Pattern Matching/enums-example/src/main.rs
+++ Chapter 6 - Enums and Pattern Matching/enums-example/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    #[derive(Debug)]
+    enum IpAddr {
+        V4(String),
+        V6(String),
+    }
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    println!("{:?}", home);
+    println!("{:?}", loopback);
+
+
+    // Other Example
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32},
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+    // impl Message {
+    //     fn call(&self) {
+    //         // Method for calling would be defined in here...
+    //     }
+    // }
+
+
+    // Example of Option - There are no 'nulls' in Rust!
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
 }
