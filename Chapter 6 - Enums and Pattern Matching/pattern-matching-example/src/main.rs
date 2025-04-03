560fd2cc18fcdc48fb185dc5f09933205b15eb5c
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-04-03 12:55:07 +0100

    Pattern matching with enums

--- Chapter 6 - Enums and Pattern Matching/pattern-matching-example/src/main.rs
+++ Chapter 6 - Enums and Pattern Matching/pattern-matching-example/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky penny!");
+                1
+            }
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
 }
