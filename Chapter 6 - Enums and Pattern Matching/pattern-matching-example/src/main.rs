beeae78716c3d53b8517f4de6a45bfca1c5ecc9b
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-04-03 13:01:09 +0100

    Did more of the pattern matching examples

--- Chapter 6 - Enums and Pattern Matching/pattern-matching-example/src/main.rs
+++ Chapter 6 - Enums and Pattern Matching/pattern-matching-example/src/main.rs
@@ -17,4 +17,36 @@ fn main() {
             Coin::Quarter => 25,
         }
     }
+
+
+    // Option Pattern Matching
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    // Matches are exhaustive, must cover all possible values!!! E.g. with these two options for 'catch-all'
+    let dice_roll = 9;
+
+    match dice_roll {
+        3 => println!("Wow, number 3!!"),
+        7 => println!("Lucky number 7!!"),
+        other => println!("Boring..."), // Will pass through the value of itself when using other.
+    }
+    
+    let dice_roll2 = 6;
+
+    match dice_roll2 {
+        3 => println!("Wow, number 3!!"),
+        7 => println!("Lucky number 7!!"),
+        _ => println!("Boring..."), // Won't pass through another value, just run what's on the left.
+    }
+
+    // TODO: Write about the 'if let' clauses
 }
